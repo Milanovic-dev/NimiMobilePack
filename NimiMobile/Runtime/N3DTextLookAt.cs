3f7d911dc5c3448dfff440c3314ff138eaeceb8c
Nikola Milanovic <57002583+Milanovic-dev@users.noreply.github.com>
2020-02-18 13:45:34 +0100

    Initial commit

---
+++ NimiMobile/Runtime/N3DTextLookAt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class N3DTextLookAt : MonoBehaviour
+{
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = cam.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cam.transform.position - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
